4dfb6b7df02b6c3663a97d993b2a58ca91c9d97f
Abdul Haseeb <mohammedabdulhaseeb4@gmail.com>
2022-06-09 22:44:32 +0530

    first commit

---
+++ src/components/JobList.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { useState } from "react";
+import "../App.css";
+import UserData from "./UserData";
+
+function JobList() {
+  const [items, setItems] = useState([]);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  fetch("http://refertest.pythonanywhere.com/job/openings")
+    .then((res) => res.json())
+    .then((json) => {
+      setItems(json);
+      setDataLoaded(true);
+    });
+  if (!dataLoaded) {
+    return (
+      <div>
+        <h1>Please wait some time....</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {" "}
+      <h1>Job Openings</h1>
+      <h3>Profile</h3>
+      <UserData />
+      <div className="row">
+        {items["data"].map((item, index) => {
+          let exp = item.min_experience;
+          return (
+            <div className="col col-lg-6">
+              <h3>Post-{index + 1}</h3>
+              <li>
+                {" "}
+                <strong>Designation:</strong> {item.designation}
+              </li>
+              <li>
+                {" "}
+                <strong>Company:</strong>
+                {item.company}{" "}
+              </li>
+              <li>
+                {" "}
+                <strong>Job location:</strong> {item.location}
+              </li>
+              <li>
+                {" "}
+                <strong>Experience:</strong>
+                {exp > 0}? {item.min_experience}:{"Fresher opening"}
+              </li>
+              <li>
+                {" "}
+                <strong>Skills required:</strong>{" "}
+                {item.skills.map((skill) => {
+                  return skill + ",";
+                })}
+              </li>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default JobList;
---
+++ src/components/UserData.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useState } from "react";
+import "../App.css";
+
+function UserData() {
+  const [userData, setUserData] = useState("");
+  const [dataLoaded, setDataLoaded] = useState(false);
+  fetch("http://refertest.pythonanywhere.com/user/data")
+    .then((res) => res.json())
+    .then((json) => {
+      setUserData(json);
+      setDataLoaded(true);
+    });
+
+  if (!dataLoaded) {
+    return (
+      <div>
+        <h1>Loading profile wait some time....</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="profile">
+      {
+        <div className="userDetails">
+          <img
+            className="profile-pic"
+            src={userData["data"].pictureUrl}
+            alt="profile-pic"
+          />
+          <hr />
+          <li> <strong>Name</strong> :{userData["data"].name}</li>
+          <li> <strong>College</strong> :{userData["data"].college}</li>
+        </div>
+      }
+    </div>
+  );
+}
+
+export default UserData;
